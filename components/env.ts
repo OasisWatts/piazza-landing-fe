8bc2cb0d599bbfdb725b28f60043431895722f44
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-06-18 00:35:46 +0900

    minor

--- components/env.ts
+++ components/env.ts
@@ -1,2 +1,2 @@
-// export const WAITLIST_SERVER_URL = "https://piazza.run" // prod
-export const WAITLIST_SERVER_URL = "http://localhost:4416" // dev
+export const WAITLIST_SERVER_URL = "https://piazza.run" // prod
+// export const WAITLIST_SERVER_URL = "http://localhost:4416" // dev
