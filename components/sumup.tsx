4aae79f63b963bc4af655cfffd1f75e7114f3f46
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-06-19 20:02:55 +0900

    minor

--- components/sumup.tsx
+++ components/sumup.tsx
@@ -3,7 +3,7 @@ export default function SumUp() {
         <section className='relative'>
             <div className="mt-[5rem] md:mt-[10rem] px-5 md:mx-auto max-w-5xl">
                 <div className="text-lg md:text-2xl font-semibold mb-[1rem] md:mb-[3rem] md:text-center" data-aos="zoom-y-out">
-                    We support <div className="round-underline round-underline-lightviolet">Information Seekers</div><br className="max-md:hidden" /> in <p className="round-underline round-underline-lightbrown"> Remembering, Organizing, and Sharing</p> web contents and their thoughts to enhance web search experience.
+                    We support <div className="round-underline round-underline-lightviolet">Information Seekers</div><br className="max-md:hidden" /> in <p className="round-underline round-underline-lightbrown"> Remembering, Organizing, and Sharing</p> web contents and their thoughts to enhance the web search experience.
                 </div>
             </div>
         </section>
