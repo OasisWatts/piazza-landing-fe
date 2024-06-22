993be796175c16fa2492d87aa2b0a972dbddf4a7
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-06-23 06:53:01 +0900

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './css/style.css'
 
 import { Nunito, Rubik } from 'next/font/google'
@@ -29,6 +30,15 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
         </div>
+        <Script type="text/javascript">
+          {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "mw4hrrn53o");
+    `}
+        </Script>
       </body>
     </html>
   )
